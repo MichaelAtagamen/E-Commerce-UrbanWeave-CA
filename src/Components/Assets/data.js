48471221eb8861f888f6a108a185d40d31cc0cba
ehima <ehimareataga063@gmail.com>
2024-02-28 00:17:10 +0000

    Updated version of UrbanWeave

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,38 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+    {
+        id:1,
+        name:"Product 1",
+        image:p1_img,
+        new_price:50.00,
+        old_price:80.50,
+    },
+    {
+        id:2,
+        name:"Product 2",
+        image:p2_img,
+        new_price:50.00,
+        old_price:80.50,
+    },
+    {
+        id:3,
+        name:"Product 3",
+        image:p3_img,
+        new_price:50.00,
+        old_price:80.50,
+    },
+    {
+        id:4,
+        name:"Product 4",
+        image:p4_img,
+        new_price:50.00,
+        old_price:80.50,
+    },
+    
+];
+
+export default data_product;
